5e56b7e47ef05696884ac678cf3e7328961eb2dc
Sanger Steel <sangersteel@gmail.com>
2025-06-01 02:21:22 -0400

    chore: Fix comment

--- modelversus/__init__.py
+++ modelversus/__init__.py
@@ -2,6 +2,6 @@ try:
     import modelversus._rust as _rust
 except ModuleNotFoundError:
     # For some reason maturin may insist on placing
-    # .so files in its own dir in site=packages/
-    # for MacOS users, so try this:
+    # .so files in their own dirs in site_packages/
+    # for MacOS users, so try this in case
     from _rust import _rust
